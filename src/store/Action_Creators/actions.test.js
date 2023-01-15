f610366a27b958c2fd35f01e4c738c74a40e7bc8
Kevin Romero Villa <kromerov1@outlook.com>
2023-01-14 20:39:34 -0500

    test asincronico y mock

--- src/store/Action_Creators/actions.test.js
+++ src/store/Action_Creators/actions.test.js
@@ -1,5 +1,11 @@
 import { ADVERTS_LOADED_SUCCESS } from "../types/types";
-import { advertsLoadedSuccess } from "./actions";
+import {
+  advertsLoadedSuccess,
+  authLogin,
+  authLoginFailure,
+  authLoginRequest,
+  authLoginSuccess,
+} from "./actions";
 
 describe("advertsLoadedSuccess", () => {
   test('Should return a "ADVERTS_LOADED_SUCCESS" action', () => {
@@ -10,5 +16,35 @@ describe("advertsLoadedSuccess", () => {
     };
     const action = advertsLoadedSuccess(adverts);
     expect(action).toEqual(expectedAction);
+  });
+});
+
+describe("authLogin", () => {
+  const credentials = "credential";
+  const action = authLogin(credentials);
+  const dispatch = jest.fn();
+  const api = { auth: {} };
+  const router = { navigate: jest.fn(), state: { location: {} } };
+
+  describe("when login api resolves", () => {
+    test("should follow the login flow", async () => {
+      api.auth.login = jest.fn().mockResolvedValue();
+      await action(dispatch, undefined, { api, router });
+      expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+      expect(api.auth.login).toHaveBeenCalledWith(credentials);
+      expect(dispatch).toHaveBeenNthCalledWith(2, authLoginSuccess());
+      expect(router.navigate).toHaveBeenCalledWith("/", { replace: true });
+    });
+  });
+
+  describe("When login api rejects", () => {
+    const error = "error";
+    test("should follow the error flow", async () => {
+      api.auth.login = jest.fn().mockRejectedValue(error);
+      await action(dispatch, undefined, { api });
+      expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+      expect(api.auth.login).toHaveBeenCalledWith(credentials);
+      expect(dispatch).toHaveBeenNthCalledWith(2, authLoginFailure(error));
+    });
   });
 });
